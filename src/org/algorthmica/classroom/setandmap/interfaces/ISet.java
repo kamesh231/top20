5506d28beb868b06426307d286e606917dd8714d
kamesh <kamesh231@gmail.com>
2017-02-21 07:53:35 +0530

    Initial Commit

---
+++ src/org/algorthmica/classroom/setandmap/interfaces/ISet.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.algorthmica.classroom.setandmap.interfaces;
+
+/**
+ * @author Kaamesh
+ *
+ */
+public interface ISet {
+	boolean add(Integer key);
+	boolean contains(Integer key);
+	boolean remove(Integer key);
+	int size();
+	void display();
+
+}
