543f4d09eae4d4e23d721a95bfc7cb0980d9be34
kamesh <kamesh231@gmail.com>
2017-02-23 07:43:51 +0530

    initcmmit

--- src/org/algorthimica/classroom/recursion/Power.java
+++ src/org/algorthimica/classroom/recursion/Power.java
@@ -16,6 +16,18 @@ public class Power {
 		  }
 		  return res;
 	  }
+	  
+	  public static long pow2(int x,int n) {
+		  if (n==0)return 1;
+		  if (n==1) return x;
+		  long tmp= pow2(x,n/2);
+		  if (n%2 ==0) {
+			  return tmp*tmp;
+		  }
+		  else 
+			  return tmp*tmp*x;
+		  
+	  }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//int a= 2147483647;
